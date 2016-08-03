a1903462bafd3bfbecd78a84e9f9940a70f89461
dawn <250810491@qq.com>
2016-08-03 08:01:37 +0800

    Update View.php

--- View.php
+++ View.php
@@ -5,55 +5,61 @@ class View
      * 模板文件路径
      * @var string
      */
-    private $viewPath;
+    protected $viewPath;
 
     /**
      * 编译文件保存路径
      * @var string
      */
-    private $cachePath;
+    protected $cachePath;
 
     /**
      * 模板文件后缀名
      * @var string
      */
-    private $viewFileSuffix = ['.html','.php'];
+    protected $viewFileSuffix = ['.html','.php'];
 
     /**
      * 保存渲染的变量
      * @var array
      */
-    private $vars = [];
+    protected $vars = [];
 
     /**
-     * 保存模板缓冲
+     * 当前模板真实路径
      * @var string
      */
-    private $viewFileBuffer;
+    protected $viewFileRealPath;
 
     /**
-     * 模板真实路径
+     * 当前编译文件路径
      * @var string
      */
-    private $viewFileRealPath;
-
-    /**
-     * 编译文件路径
-     * @var string
-     */
-    private $compileFilePath;
+    protected $compileFilePath;
 
     /**
      * 保存yield section对应关系
      * @var array
      */
-    private $sections = [];
+    protected $sections = [];
+
+    /**
+     * section堆
+     * @var array
+     */
+    protected $sectionStack = [];
+
+    /**
+     * 记录编译次数
+     * @var int
+     */
+    protected $compileCount = 0;
 
     /**
      * 替换规则
      * @var array
      */
-    private $replaces = [
+    protected $rules = [
         "/{{\s*(.*?)\s*}}/" => '<?php echo $this->e(${1}); ?>',
         "/{!!\s*(.*?)\s*!!}/" => '<?php echo ${1}; ?>',
         "/{{--\s*(.*?)\s*--}}/" => '<?php /* ${1} */ ?>',
@@ -66,20 +72,226 @@ class View
     ];
 
     /**
-     * 编译文件缓存时间
-     * @var int
-     */
-    private $cacheExpire = 0;
-
-    /**
      * View 构造函数
-     * @param $viewPath Template File Path
-     * @param $cachePath Compile File Cache Path
+     * @param $viewPath
+     * @param $cachePath
      */
     public function __construct($viewPath, $cachePath)
     {
         $this->viewPath = $viewPath;
         $this->cachePath = $cachePath;
+    }
+
+    /**
+     * 使用模板
+     * @param $file
+     * @param array $vars
+     * @return $this
+     */
+    public function make($file, $vars = [])
+    {
+        //保存分配变量
+        $this->vars = array_merge($this->vars, $vars);
+        //获得文件真实路径
+        $this->viewFileRealPath = $this->getViewFileRealPath($file);
+        //获得编译文件路径
+        $this->compileFilePath = $this->getCompileFilePath($this->viewFileRealPath);
+
+        return $this;
+    }
+
+    /**
+     * 渲染
+     * @return string
+     */
+    public function render()
+    {
+        //进行编译
+        $this->compile();
+        //解析变量
+        extract($this->vars);
+
+        if ($this->compileCount == 0) {
+            ob_start();
+        }
+        $this->compileCount++;
+
+        require $this->compileFilePath;
+
+        if ($this->compileCount == 1) {
+            $result = ob_get_clean();
+        }
+        return isset($result) ? $result : '';
+    }
+
+    /**
+     * 判断编译文件是否过期
+     * @return bool
+     */
+    protected function isExpired()
+    {
+        if (! is_file($this->compileFilePath)) {
+            return true;
+        }
+
+        $cacheLastModify = filemtime($this->compileFilePath);
+        $viewLastModify = filemtime($this->viewFileRealPath);
+
+        if ($cacheLastModify <= $viewLastModify) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 编译模板
+     */
+    protected function compile()
+    {
+        if($this->isExpired()) {
+            $buffer = file_get_contents($this->viewFileRealPath);
+            $buffer = $this->compileExtends($buffer);
+            $buffer = $this->compileInclude($buffer);
+            $buffer = $this->compileSection($buffer);
+            $buffer = $this->compileYield($buffer);
+            $buffer = $this->compileEcho($buffer);
+            $this->putBufferToCompileFile($buffer);
+        }
+    }
+
+    /**
+     * 编译extends
+     * @param $buffer
+     * @return string
+     */
+    protected function compileExtends($buffer)
+    {
+        $pattern = "/@extends\(['|\"](.+?)['|\"]\)/";
+
+        preg_match_all($pattern, $buffer, $matches);
+
+        if(!empty($matches[0])) {
+            foreach ($matches[1] as $value) {
+                $buffer .= PHP_EOL .'<?php $this->make("'. $value .'",$this->vars)->render();?>';
+            }
+        }
+
+        $buffer = preg_replace($pattern, '', $buffer);
+
+        return trim($buffer);
+    }
+
+    /**
+     * 编译include
+     * @param $buffer
+     * @return string
+     */
+    protected function compileInclude($buffer)
+    {
+        $pattern = "/@include\(['|\"](.+?)['|\"]\)/";
+
+        $buffer = preg_replace_callback($pattern, function($matches){
+            return '<?php $this->make("'. $matches[1] .'",$this->vars)->render(); ?>';
+        }, $buffer);
+
+        return trim($buffer);
+    }
+
+    /**
+     * 编译section
+     * @param $buffer
+     * @return string
+     */
+    protected function compileSection($buffer)
+    {
+        $pattern = "/@section\(['|\"](.+?)['|\"]\)([\s\S]+?)@(stop|show)/";
+        $buffer = preg_replace_callback($pattern, function($matches){
+            $str = PHP_EOL . '<?php $this->startSection("'. $matches[1] .'");?>'
+                . PHP_EOL
+                . $matches[2]
+                . PHP_EOL;
+            if ($matches[3] == 'show') {
+                $str .= '<?php $this->stopSection(true); ?>' . PHP_EOL;
+            } else {
+                $str .= '<?php $this->stopSection(); ?>' . PHP_EOL;
+            }
+            return $str;
+        }, $buffer);
+
+        return trim($buffer);
+    }
+
+    /**
+     * 开始section
+     * @param $name
+     */
+    protected function startSection($name)
+    {
+        if(ob_start()) {
+            $this->sectionStack[] = $name;
+        }
+    }
+
+    /**
+     * 结束section
+     * @param bool $show
+     */
+    protected function stopSection($show = false)
+    {
+        $buffer = ob_get_clean();
+
+        $last = array_pop($this->sectionStack);
+
+        if(!isset($this->sections[$last])) {
+            if ($show) {
+                echo $buffer;
+            } else {
+                $this->sections[$last] = $buffer;
+            }
+        } else {
+            echo $this->sections[$last];
+        }
+    }
+
+    /**
+     * 编译yield
+     * @param $buffer
+     * @return string
+     */
+    protected function compileYield($buffer)
+    {
+        $pattern = "/@yield\(['|\"](.+?)['|\"]\)/";
+        $buffer = preg_replace_callback($pattern, function($matches) {
+            if (isset($this->sections[$matches[1]])) {
+                return $this->sections[$matches[1]];
+            } else {
+                return '';
+            }
+        }, $buffer);
+
+        return trim($buffer);
+    }
+
+    /**
+     * 编译输出
+     * @param $buffer
+     * @return mixed
+     */
+    protected function compileEcho($buffer)
+    {
+        $buffer = preg_replace(array_keys($this->rules), array_values($this->rules), $buffer);
+        return $buffer;
+    }
+
+    /**
+     * 将缓冲内容放到编译文件中
+     * @param $buffer
+     * @return int
+     */
+    protected function putBufferToCompileFile($buffer)
+    {
+        return file_put_contents($this->compileFilePath, $buffer, LOCK_EX);
     }
 
     /**
@@ -92,190 +304,6 @@ class View
     {
         $this->vars[$key] = $value;
         return $this;
-    }
-
-    /**
-     * 初始化模板信息
-     * @param $fileName
-     * @return $this
-     */
-    public function make($fileName)
-    {
-        $this->viewFileRealPath = $this->getViewFileRealPath($fileName);
-        $this->compileFilePath = $this->getCompileFilePath($this->viewFileRealPath);
-
-        if ($this->isExpireOut()) {
-            $this->setViewFileBuffer($fileName);
-        }
-
-        return $this;
-    }
-
-    /**
-     * 渲染
-     * @return string
-     */
-    public function render()
-    {
-        $this->compile();
-        extract($this->vars);
-
-        ob_start();
-        require $this->compileFilePath;
-        return ob_get_clean();
-    }
-
-    /**
-     * 添加模板文件后缀
-     * @param $suffix
-     * @return int
-     */
-    public function addViewFileSuffix($suffix)
-    {
-        return array_unshift($this->viewFileSuffix, $suffix);
-    }
-
-    /**
-     * 添加规则
-     * @param $preg
-     * @param $relacement
-     */
-    public function addRule($preg, $relacement)
-    {
-        $this->replaces[$preg] = $relacement;
-    }
-
-    /**
-     * 设置编译缓存时间
-     * @param $second
-     */
-    public function setCacheExpire($second)
-    {
-        $this->cacheExpire = $second;
-    }
-
-    /**
-     * 判断缓存是否过期
-     * @return bool
-     */
-    private function isExpireOut()
-    {
-        if (is_file($this->compileFilePath)) {
-            if ($this->cacheExpire == 0) {
-                return true;
-            }
-            $createTime = filectime($this->compileFilePath);
-            if (($createTime + $this->cacheExpire) < time()) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * 设置文件缓冲
-     * @param $fileName
-     */
-    private function setViewFileBuffer($fileName)
-    {
-        $buffer = file_get_contents($this->getViewFileRealPath($fileName));
-
-        while(1) {
-            preg_match("/(@yield|@include|@section|@extends)/", $buffer, $matches);
-            if (! empty($matches)) {
-                $buffer = $this->dealExtends($buffer);
-                $buffer = $this->dealYield($buffer);
-                $buffer = $this->dealInclude($buffer);
-                $buffer = $this->dealSection($buffer);
-            } else {
-                break;
-            }
-        }
-
-        $this->viewFileBuffer = $buffer;
-    }
-
-    /**
-     * 处理section的继承
-     * @param $buffer
-     * @return mixed
-     */
-    private function dealSection($buffer)
-    {
-        $result = preg_replace_callback("/@section\(['|\"](.*?)['|\"]\)([\s\S]*?)@stop/", function($matches) {
-            if (!isset($this->sections[$matches[1]])) {
-                $this->sections[$matches[1]] = $matches[2];
-                return $matches[0];
-            } else {
-                return $this->sections[$matches[1]];
-            }
-        }, $buffer);
-
-        return $result;
-    }
-
-    /**
-     * 用section内容替换yield处
-     * @param $buffer
-     * @return mixed
-     */
-    private function dealYield($buffer)
-    {
-        $result = preg_replace_callback("/@yield\(('|\")(.*?)('|\")\)/", function($matches) {
-            if (!empty($this->sections[$matches[2]])) {
-                return $this->sections[$matches[2]];
-            }
-            return '';
-        }, $buffer);
-
-        return $result;
-    }
-
-    /**
-     * 处理继承关系
-     * @param $buffer
-     * @return string
-     */
-    private function dealExtends($buffer)
-    {
-        //匹配section
-        preg_match_all("/@section\(['|\"](.*?)['|\"]\)([\s\S]*?)@stop/", $buffer, $matchSection);
-        if (!empty($matchSection[0])) {
-            foreach ($matchSection[1] as $key => $value) {
-                if (empty($this->sections[$value])) {
-                    $this->sections[$value] = $matchSection[2][$key];
-                }
-            }
-        }
-
-        //继承匹配
-        preg_match("/@extends\(('|\")(.*?)('|\")\)/", $buffer, $matchExtends);
-        if (!empty($matchExtends)) {
-            $bufferTemp = file_get_contents($this->getViewFileRealPath($matchExtends[2]));
-            return $this->dealExtends($bufferTemp);
-        }
-
-        return $buffer;
-    }
-
-    /**
-     * 获得需要引入的文件
-     * @param $buffer
-     * @return bool
-     */
-    private function dealInclude($buffer)
-    {
-        //判断是否有引入文件
-        $result = preg_replace_callback("/@include\(('|\")(.*?)('|\")\)/", function($matches) {
-            $bufferTemp = file_get_contents($this->getViewFileRealPath($matches[2]));
-            //递归查找引入文件
-            return $this->dealInclude($bufferTemp);
-        }, $buffer);
-
-        return $result;
     }
 
     /**
@@ -299,6 +327,16 @@ class View
     }
 
     /**
+     * 安全输出变量
+     * @param $value
+     * @return string
+     */
+    protected function e($value)
+    {
+        return htmlentities($value, ENT_QUOTES, 'UTF-8', false);
+    }
+
+    /**
      * 获得编译文件的完整名
      * @return string
      */
@@ -308,27 +346,22 @@ class View
     }
 
     /**
-     * 编译
-     * @return int
+     * 添加模板后缀名
+     * @param $suffix
      */
-    private function compile()
+    public function addViewFileSuffix($suffix)
     {
-        if ($this->isExpireOut()) {
-            //执行编译替换
-            $buffer = preg_replace(array_keys($this->replaces), array_values($this->replaces), $this->viewFileBuffer);
-
-            return file_put_contents($this->compileFilePath, $buffer, LOCK_EX);
-        }
+        array_unshift($this->viewFileSuffix, $suffix);
     }
 
     /**
-     * 安全输出变量
-     * @param $value
-     * @return string
+     * 添加自定义规则
+     * @param $rule
+     * @param $replace
      */
-    private function e($value)
+    public function addRule($rule, $replace)
     {
-        return htmlspecialchars($value);
+        $this->rules[$rule] = $replace;
     }
 
     /**
@@ -336,7 +369,7 @@ class View
      * @param $str
      * @return string
      */
-    private function snakeCase($str)
+    protected function snakeCase($str)
     {
         $array = array();
         $len = strlen($str);
